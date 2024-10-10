1297dc47e3ff0b49b601a23adf5a61242132e7b5
NaSeEb <NaSeEb@DESKTOP-QTFR6NJ>
2024-10-10 13:11:16 -0700

    Hello

--- shefeenaproj1/src/main/java/project1/Display.java
+++ shefeenaproj1/src/main/java/project1/Display.java
@@ -4,7 +4,8 @@ public class Display {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello hishefeena");
+		System.out.println("Hello");
+		
 	}
 
 }
