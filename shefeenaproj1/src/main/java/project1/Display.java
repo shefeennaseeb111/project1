dddf226027e26abb8a1c78c562074f10d3d29e2a
NaSeEb <NaSeEb@DESKTOP-QTFR6NJ>
2024-10-10 12:52:40 -0700

    hid

--- shefeenaproj1/src/main/java/project1/Display.java
+++ shefeenaproj1/src/main/java/project1/Display.java
@@ -4,7 +4,7 @@ public class Display {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("shefeena");
+		System.out.println("Hi shefeena");
 	}
 
 }
