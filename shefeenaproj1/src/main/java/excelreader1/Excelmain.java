75094ee08cb5ba88658927cf699d021037a7c29f
NaSeEb <NaSeEb@DESKTOP-QTFR6NJ>
2024-10-22 08:43:29 -0700

    proj1

---
+++ shefeenaproj1/src/main/java/excelreader1/Excelmain.java
@@ -0,0 +1,13 @@
+package excelreader1;
+
+import java.io.IOException;
+
+public class Excelmain {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println(Excelcode.getStringData(1, 1));
+		System.out.println(Excelcode.getIntegerData(1, 0));
+	}
+
+}
